624bb5350f4b3b42ee32bb030f17b30de82373db
Katie <hall.kaitlin.id@gmail.com>
2017-04-24 14:31:45 -0600

    receiversynthesizing properly

--- Receiver.v
+++ Receiver.v
@@ -53,12 +53,30 @@ module Receiver(
 		begin
 			counter <= counter + 1'b1; 
 		end
+		
+		case(state)
+			1: RCV_DATA[0] = RCV; 
+			2: RCV_DATA[1] = RCV; 
+			3: RCV_DATA[2] = RCV; 
+			4: RCV_DATA[3] = RCV; 
+			5: RCV_DATA[4] = RCV; 
+			6: RCV_DATA[5] = RCV; 
+			7: RCV_DATA[6] = RCV; 
+			8:	RCV_DATA[7] = RCV; 
+			default: begin
+				//RCV_DATA = 0; 
+			end
+		
+		endcase
+		
 	end
 	
 	always@(*)
 	begin
+	
 		case(state)
 			0: begin
+				RCV_REQ = 0; 
 				if(!RCV) begin
 					next_state = 1; 
 				end
@@ -68,66 +86,85 @@ module Receiver(
 			end
 			
 			1: begin
-				RCV_DATA[0] = RCV; 
+				RCV_REQ = 0; 
+				//RCV_DATA[0] = RCV; 
 				next_state = 2; 
 			end
 			
 			2: begin
-				RCV_DATA[1] = RCV; 
+				RCV_REQ = 0; 
+				//RCV_DATA[1] = RCV; 
 				next_state = 3; 
 			end
 			
 			3: begin
-				RCV_DATA[2] = RCV; 
+				RCV_REQ = 0; 
+				//RCV_DATA[2] = RCV; 
 				next_state = 4; 
 			end
 			
 			4: begin
-				RCV_DATA[3] = RCV; 
+				RCV_REQ = 0; 
+				//RCV_DATA[3] = RCV; 
 				next_state = 5; 
 				
 			end
 			
 			5: begin
-				RCV_DATA[4] = RCV; 
+				RCV_REQ = 0; 
+				//RCV_DATA[4] = RCV; 
 				next_state = 6; 
 			end
 			
 			6: begin
-				RCV_DATA[5] = RCV; 
+				RCV_REQ = 0; 
+				//RCV_DATA[5] = RCV; 
 				next_state = 7; 
 			end
 			
 			7: begin
-				RCV_DATA[7] = RCV; 
+				RCV_REQ = 0; 
+				//RCV_DATA[6] = RCV; 
 				next_state = 8; 
 			end
 			
 			8: begin
-				RCV_REQ = 1; 
-				if(RCV_ACK) begin
-					next_state = 9; 
-				end
-				next_state = 8; 
+				RCV_REQ = 0; 
+				//RCV_DATA[7] = RCV;  
+				next_state = 9; 
 			end
 			
 			9: begin
+				RCV_REQ = 1; 
+				if(RCV_ACK) begin
+					next_state = 10; 
+				end
+				next_state = 9; 
+			end
+			
+			10: begin
 				RCV_REQ = 0; 
 				if(!RCV_ACK) begin
 					next_state = 0; 
 				end
 				else begin
-					next_state = 9; 
+					next_state = 10; 
 				end
+			
 			end
+	
 			default: begin
 				RCV_REQ = 0; 
-				RCV_DATA = 0; 
+				//RCV_DATA = 0; 
 				next_state = 0; 
 			end
 		
 		endcase
 	
 	end
+	
+	/*Hardest part is calculating how long to wait 
+	* Simulate TX line a few lines
+	* simulate transmit and see what happens*/
 
 endmodule
